a4df5b8b6f5281bc9d941bab948c66e6adbf8f0c
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-07-12 12:11:26 -0500

    agregando estilos al user

--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import { useParams, Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import useRequest from '../../hooks/useRequest';
+import {Container, Span, Img, Name, P, ContainerText, Button} from './UserStyles';
 
 const User = () => {
+    const navigate = useNavigate();
     const { login } = useParams();
     const url = `https://api.github.com/users/${login}`;
     const user = useRequest(url)
-
   return (
-    <div>
+    <>
         {
             user &&
-            <div>
-                <h1>{user.login}</h1>
-                <img src={user.avatar_url} alt={user.login} />
+            <Container>
                 <div>
-                    <Link to="/home">Volver</Link>
+                  <Name>{user.login}</Name>
+                  <Img src={user.avatar_url} alt={user.login} />
                 </div>
-            </div>
+                <ContainerText>
+                  <P><Span>Name:</Span> {user.name}</P>
+                  <P><Span>URL GitHub:</Span> <a href={user.url}>{user.login}</a></P>
+                  <P><Span>Location:</Span> {user.location}</P>
+                  {user.company === null ? null : <P><Span>Company:</Span> {user.company}</P>}
+                  {user.bio === null ? null : <P><Span>Bio:</Span> {user.bio}</P>}
+                  
+                </ContainerText>
+            </Container>
           }
-    </div>
+          <Button onClick={() => navigate('/home')} >Volver</Button>
+    </>
   )
 }
 
---
+++ src/pages/user/UserStyles.js
@@ -0,0 +1,33 @@
+import { style } from "@mui/system";
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const Span = styled.span`
+    font-size: 20px;
+    font-weight: bold;
+`
+
+export const Img = styled.img`
+    border-radius: 20px;
+    margin-right: 30px;
+`
+
+export const Name = styled.h1`
+    margin: 20px 0;
+    color: white;
+    text-align: center;
+`
+
+export const P = styled.p`
+    font-size: 25px;
+    margin-bottom: 10px;
+`
+
+export const ContainerText = styled.div`
+    width: 500px;
+`
