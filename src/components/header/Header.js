fdba61fa7e8b9c67adc1ee8e8a58cd6ff56d8238
Jacobo121 <wagner.arcila.jacobo@gmail.com>
2022-07-13 10:59:43 -0500

    update

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from "react-router-dom";
 import {
   Container,
@@ -8,19 +8,35 @@ import {
   SearchButton,
 } from "./HeaderStyles";
 import { useSearch } from "../../hooks/useSearch";
+import useRequest from "../../hooks/useRequest";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Autosuggest from "react-autosuggest";
 
 const Header = () => {
+
   const { handleSubmit, setSearchInput } = useSearch();
+  const url = `https://api.github.com/users`;
+  const user = useRequest(url);
+  console.log(user)
+  const [users, setUsers] = useState(user);
+  const [value, setValue] = useState("");
+  const [selecionado, setSeleccionado] = useState({});
+
   return (
     <Container>
       <h2>Forms</h2>
       <SearchForm onSubmit={(e) => handleSubmit(e)}>
-        <SearchInput placeholder="Buscar Usuario...." onChange={(e) => setSearchInput(e.target.value)} />
+        <SearchInput
+          placeholder="Buscar Usuario...."
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
         <SearchButton type="submit">Buscar</SearchButton>
       </SearchForm>
       <Link to="/register/signIn">
         <Button>Log out</Button>
       </Link>
+
+      
     </Container>
   );
 };
